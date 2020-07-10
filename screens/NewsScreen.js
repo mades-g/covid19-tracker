90d5d54c15173623d16d31bdffa14028546cc4b9
Eudes G.G Duarte <eudes.duarte@infinity.co>
2020-07-10 11:06:02 +0100

    [Screen] - Basic setup for News screen

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Colors from '../styles/Colors';
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: Colors.colorWhite,
+        flex: 1,
+        justifyContent: 'center',
+    },
+    title: {
+        color: Colors.colorBlack,
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
 
 export function NewsScreen() {
     return (
-        <View>
-            <Text>NewsScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>News</Text>
+            <Text>...loading</Text>
         </View>
     );
 }
